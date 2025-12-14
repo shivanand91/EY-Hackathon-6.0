85b6e399243a50912dc20546af2f8e6f882de550
Shivanand Kumar <shivanand916232@gmail.com>
2025-12-15 00:40:45 +0530

    Something changes in frontend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,20 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//   baseURL: "http://localhost:8000/api/v1",
-//   headers: {
-//     "Content-Type": "application/json"
-//   }
-// });
-
-// export const diagnoseVehicle = (payload) =>
-//   api.post("/diagnose", payload);
-
-// export const fetchOEMInsights = () =>
-//   api.get("/oem/insights");
-
-// export default api;
-
 import axios from "axios";
 
 const api = axios.create({
